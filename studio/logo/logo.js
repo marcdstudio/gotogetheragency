56e6d3ef729a9d3f26ac2d95725e689c672f1629
Jake Pfahl <jakepfahl@Jakes-iMac.lan>
2020-10-09 12:38:36 -0400

    added studio styling

---
+++ studio/logo/logo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './logo.css'
+
+const logo = () => (
+  <img
+    className={styles.logo}
+    src="/static/logo_mark_tan.png"
+    alt="schulz logo"
+  />
+)
+
+export default logo
