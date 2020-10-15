9c1db89ddb030d0e45cb3dd0ecea49b8c9a45150
Jake Pfahl <jakepfahl@Jakes-iMac.lan>
2020-10-15 01:07:17 -0400

    fix page transitions

--- web/src/scripts/components/project.js
+++ web/src/scripts/components/project.js
@@ -21,21 +21,46 @@ export default component((node) => {
     if (
       $(this)
         .find('.pMouse')
-        .hasClass('pLive')
+        .hasClass('pLive') &&
+      document.documentElement.clientWidth > 1025
     ) {
       $('.workThumb')
         .not(this)
         .css('visibility', 'hidden')
       var curScroll = $('.scrollContainer').scrollTop()
       var curOffset = $(this).offset().top
-      if ($(window).width > 1025) {
-        var imgOffset = curScroll - curOffset + 40
-      } else if ($(window).width > 650) {
-        var imgOffset = curScroll - curOffset + 30
-      } else {
-        var imgOffset = curScroll - curOffset + 20
-      }
-      // Scroll.destroy()
+      var imgOffset = curScroll - curOffset + 40
+
+      $(this).css('transform', 'translateY(' + imgOffset + 'px)')
+      $(this).addClass('wtFull')
+    } else if (
+      $(this)
+        .find('.pMouse')
+        .hasClass('pLive') &&
+      document.documentElement.clientWidth > 650
+    ) {
+      $('.workThumb')
+        .not(this)
+        .css('visibility', 'hidden')
+      var curScroll = $('.scrollContainer').scrollTop()
+      var curOffset = $(this).offset().top
+      var imgOffset = curScroll - curOffset + 30
+
+      $(this).css('transform', 'translateY(' + imgOffset + 'px)')
+      $(this).addClass('wtFull')
+    } else if (
+      $(this)
+        .find('.pMouse')
+        .hasClass('pLive') &&
+      document.documentElement.clientWidth < 650
+    ) {
+      $('.workThumb')
+        .not(this)
+        .css('visibility', 'hidden')
+      var curScroll = $('.scrollContainer').scrollTop()
+      var curOffset = $(this).offset().top
+      var imgOffset = curScroll - curOffset + 20
+
       $(this).css('transform', 'translateY(' + imgOffset + 'px)')
       $(this).addClass('wtFull')
     }
